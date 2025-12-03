bdd80006282129b915df88bdee209bb85e314a52
Dhanesh <dhanesh2435@gmail.com>
2025-12-03 21:30:46 +0530

    feat:/Updated core services Docker file

--- Gateway/src/main/java/com/FitnessService/Gateway/SecurityConfig.java
+++ Gateway/src/main/java/com/FitnessService/Gateway/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.FitnessService.Gateway;
 
-import org.springframework.beans.factory.annotation.Value; // Import for injecting external property
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder; // Import the Decoder
-import org.springframework.security.oauth2.jwt.JwtDecoders; // Import the Factory
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder; 
+import org.springframework.security.oauth2.jwt.JwtDecoders; 
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
