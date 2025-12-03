1cf33d742948873759803362ade9a02292a31b7c
Dhanesh <dhanesh2435@gmail.com>
2025-12-03 21:59:13 +0530

    feat:/Updated core services Docker file

--- Gateway/src/main/java/com/FitnessService/Gateway/SecurityConfig.java
+++ Gateway/src/main/java/com/FitnessService/Gateway/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.FitnessService.Gateway;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder; 
-import org.springframework.security.oauth2.jwt.JwtDecoders; 
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
@@ -20,17 +17,9 @@ import java.util.List;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuerUri; 
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation(issuerUri); 
-    }
-
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
@@ -43,14 +32,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); 
-        
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-User-ID"));
         config.setAllowCredentials(true);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config); 
+        source.registerCorsConfiguration("/**", config);
+
         return source;
     }
 }
